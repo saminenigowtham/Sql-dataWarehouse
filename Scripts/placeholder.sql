7e7471caa6246056174ce464c74837b1861d730b
saminenigowtham <122296497+saminenigowtham@users.noreply.github.com>
2025-03-17 11:18:40 +0530

    Update placeholder.sql

--- Scripts/placeholder.sql
+++ Scripts/placeholder.sql
@@ -1,1 +1,22 @@
+/*
+scripts :
+	we are creating the database with named as datawarhouse and creating the schema as bronze,silver and gold
+
+
+
+*/
+
+use master;
+-- creating datbase
+create database DataWarhouse;
+-- using the data base
+use DataWarhouse;
+
+-- createing schema in database
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
 
