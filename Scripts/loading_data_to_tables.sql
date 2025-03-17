5f512597a8b77b69c29a4143b2dc147ed6185edd
saminenigowtham <122296497+saminenigowtham@users.noreply.github.com>
2025-03-17 14:25:29 +0530

    Update loading_data_to_tables.sql

--- Scripts/loading_data_to_tables.sql
+++ Scripts/loading_data_to_tables.sql
@@ -52,3 +52,16 @@ firstrow = 2,
 fieldterminator = ',',
 tablock
 );
+-- seeeing the data 
+
+select * from bronze.crm_cust_info;
+
+select * from bronze.crm_prd_info;
+
+select * from bronze.crm_sales_details;
+
+select * from [bronze].[erp_cust_az12];
+
+select * from [bronze].[erp_loc_a101];
+
+select * from [bronze].[erp_px_cat_g1v2];
